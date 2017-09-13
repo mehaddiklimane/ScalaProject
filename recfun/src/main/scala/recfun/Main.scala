b240981a5c7e49d4b754d7bc7804a8db0119a321
mehaddiklimane <31927091+mehaddiklimane@users.noreply.github.com>
2017-09-13 22:54:33 +0200

    Add files via upload

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -43,31 +43,9 @@ object Main {
     * Exercise 3
     */
   def countChange(money: Int, coins: List[Int]): Int = {
-    def aux(lastMaxCoin_total_coll: List[(Int, Int)], count: Int): Int = {
-      if (lastMaxCoin_total_coll.isEmpty) {
-        count
-      } else {
-        val b = ListBuffer[(Int, Int)]()
-        var newCount = count
-        for ((lastMaxCoin, total) <- lastMaxCoin_total_coll) {
-          if (total < money) {
-            for (c <- coins) {
-              if (c >= lastMaxCoin) {
-                val e = (c, total + c)
-                b += e
-              }
-            }
-          } else if (total == money) {
-            newCount += 1
-          }
-        }
-
-        aux(b.toList, newCount)
-      }
-    }
-
-    val b = coins.map { c => (c, c) }
-    aux(b, 0)
+    if (money == 0) 1
+    else if (coins.isEmpty || money < 0) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
 
