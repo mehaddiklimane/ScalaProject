3fdf78346a0eb5e4bf406a7764b205681fd515df
mehaddiklimane <31927091+mehaddiklimane@users.noreply.github.com>
2017-09-13 17:49:52 +0200

    Add files via upload

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -22,28 +22,28 @@ object Main {
     * Exercise 2
     */
   def balance(chars: List[Char]): Boolean = {
-    def f(chars: List[Char], numOpens: Int): Boolean = {
+    def aux(chars: List[Char], bracketOpens: Int): Boolean = {
       if (chars.isEmpty) {
-        numOpens == 0
+        true
       } else {
         val h = chars.head
         val n =
-          if (h == '(') numOpens + 1
-          else if (h == ')') numOpens - 1
-          else numOpens
-        if (n >= 0) f(chars.tail, n)
+          if (h == '(') bracketOpens + 1
+          else if (h == ')') bracketOpens - 1
+          else bracketOpens
+        if (n >= 0) aux(chars.tail, n)
         else false
       }
     }
 
-    f(chars, 0)
+    aux(chars, 0)
   }
 
   /**
     * Exercise 3
     */
   def countChange(money: Int, coins: List[Int]): Int = {
-    def f(lastMaxCoin_total_coll: List[(Int, Int)], count: Int): Int = {
+    def aux(lastMaxCoin_total_coll: List[(Int, Int)], count: Int): Int = {
       if (lastMaxCoin_total_coll.isEmpty) {
         count
       } else {
@@ -62,12 +62,12 @@ object Main {
           }
         }
 
-        f(b.toList, newCount)
+        aux(b.toList, newCount)
       }
     }
 
     val b = coins.map { c => (c, c) }
-    f(b, 0)
+    aux(b, 0)
   }
 }
 
